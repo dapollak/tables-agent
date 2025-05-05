353fb0fbbe35f72e3d2d096df28d3a5d0f5c8bd8
Daniel Pollak <daniel@elementary-data.com>
2025-05-05 16:25:54 +0300

    .

--- api/index.py
+++ api/index.py
@@ -1,1 +1,26 @@
-from api.main import app
+from fastapi import FastAPI
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+
+from api.agent import get_number
+from api.schemas import Tables
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/api/get-table-number/{name}", response_model=Tables)
+async def get_table_number(name: str) -> Tables:
+    return await get_number(name)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
